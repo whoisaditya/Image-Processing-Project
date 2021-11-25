df8bc19169b9783de7b33b9222d2b22bbf52ca77
Aditya Mitra <mitraaditya1357@gmail.com>
2021-11-26 03:27:14 +0530

    feat: added project dashboard

--- README.md
+++ README.md
@@ -4,12 +4,18 @@
 <tr align="center">
 
 
-<td>
-Aditya Mitra
+## Contributors
+<table>
+  <tr>
+    <td align="center"><a href="https://github.com/AdityaDeodeshmukh"><img src="" width="200px;" alt=""/><br /><sub><b>Aditya Deodeshmukh</b></sub></a><br /> </td>
+      <td align="center"><a href="https://github.com/whoisaditya"><img src="" width="200px;" alt=""/><br /><sub><b>Aditya Mitra</b></sub></a><br /> </td></td>
+   <td align="center"><a href="https://github.com/Anniee02"><img src="./diagrams/db.png" width="200px;" alt=""/><br /><sub><b>Ananya Thakre</b></sub></a><br /> </td></td>
+   
+  </tr>
+  </table>
 
-<td>
-Ananya Thakre
 
-<td>
-Aditya Deodeshmukh
-</table>
+
+<p align="center">
+	With :heart: by US
+</p>
--- stereo_vision.py
+++ stereo_vision.py
@@ -12,10 +12,17 @@ import calibration
 # Mediapipe for face detection
 import mediapipe as mp
 import time
-
+WINDOW_SIZE=(10,1080,3)
 mp_facedetector = mp.solutions.face_detection
 mp_draw = mp.solutions.drawing_utils
-
+mid_bar=np.zeros([500,660,3])
+mid_bar.fill(150)
+window = np.zeros([1080,1600,3],dtype=np.uint8)
+window.fill(60)
+window[290:790,70:730]=mid_bar
+window[290:790,790:1450]=mid_bar
+cv2.putText(window, "Project Dashboard ", (80,100), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0,255,0),3)
+window_cpy=window
 # Open both cameras
 cap_right = cv2.VideoCapture(2, cv2.CAP_DSHOW)                    
 cap_left =  cv2.VideoCapture(1, cv2.CAP_DSHOW)
@@ -23,122 +30,124 @@ cap_left =  cv2.VideoCapture(1, cv2.CAP_DSHOW)
 
 # Stereo vision setup parameters
 frame_rate = 120    #Camera frame rate (maximum at 120 fps)
-B = 8               #Distance between the cameras [cm]
-f = 20              #Camera lense's focal length [mm]
-alpha = 60        #Camera field of view in the horisontal plane [degrees]
+B = 9               #Distance between the cameras [cm]
+f = 8              #Camera lense's focal length [mm]
+alpha = 56.6        #Camera field of view in the horisontal plane [degrees]
 
 
 
 
 # Main program loop with face detector and depth estimation using stereo vision
+with mp_facedetector.FaceDetection(min_detection_confidence=0.7) as face_detection:
 
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    while(cap_right.isOpened() and cap_left.isOpened()):
 
-while(cap_right.isOpened() and cap_left.isOpened()):
+        succes_right, frame_right = cap_right.read()
+        succes_left, frame_left = cap_left.read()
+        window=np.copy(window_cpy)
+    ################## CALIBRATION #########################################################
 
-    succes_right, frame_right = cap_right.read()
-    greyR=cv2.cvtColor(frame_right,cv2.COLOR_BGR2GRAY)
-    succes_left, frame_left = cap_left.read()
-    greyL=cv2.cvtColor(frame_left,cv2.COLOR_BGR2GRAY)
-################## CALIBRATION #########################################################
+        frame_right, frame_left = calibration.undistortRectify(frame_right, frame_left)
 
-    frame_right, frame_left = calibration.undistortRectify(frame_right, frame_left)
+    ########################################################################################
 
-########################################################################################
-
-    # If cannot catch any frame, break
-    if not succes_right or not succes_left:                    
-        break
-
-    else:
-
-        start = time.time()
-        
-        # Convert the BGR image to RGB
-        frame_right = cv2.cvtColor(frame_right, cv2.COLOR_BGR2RGB)
-        frame_left = cv2.cvtColor(frame_left, cv2.COLOR_BGR2RGB)
-
-        # Process the image and find faces
-        results_right = face_detection.process(frame_right)
-        results_left = face_detection.process(frame_left)
-
-        # Convert the RGB image to BGR
-        frame_right = cv2.cvtColor(frame_right, cv2.COLOR_RGB2BGR)
-        frame_left = cv2.cvtColor(frame_left, cv2.COLOR_RGB2BGR)
-
-
-        ################## CALCULATING DEPTH #########################################################
-
-        center_right = 0
-        center_left = 0
-
-        if results_right.detections:
-            for id, detection in enumerate(results_right.detections):
-                mp_draw.draw_detection(frame_right, detection)
-
-                bBox = detection.location_data.relative_bounding_box
-
-                h, w, c = frame_right.shape
-
-                boundBox = int(bBox.xmin * w), int(bBox.ymin * h), int(bBox.width * w), int(bBox.height * h)
-
-                center_point_right = (boundBox[0] + boundBox[2] / 2, boundBox[1] + boundBox[3] / 2)
-
-                cv2.putText(frame_right, f'{int(detection.score[0]*100)}%', (boundBox[0], boundBox[1] - 20), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,255,0), 2)
-
-
-        if results_left.detections:
-            for id, detection in enumerate(results_left.detections):
-                mp_draw.draw_detection(frame_left, detection)
-
-                bBox = detection.location_data.relative_bounding_box
-
-                h, w, c = frame_left.shape
-
-                boundBox = int(bBox.xmin * w), int(bBox.ymin * h), int(bBox.width * w), int(bBox.height * h)
-
-                center_point_left = (boundBox[0] + boundBox[2] / 2, boundBox[1] + boundBox[3] / 2)
-
-                cv2.putText(frame_left, f'{int(detection.score[0]*100)}%', (boundBox[0], boundBox[1] - 20), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,255,0), 2)
-
-
-
-
-        # If no ball can be caught in one camera show text "TRACKING LOST"
-        if not results_right.detections or not results_left.detections:
-            cv2.putText(frame_right, "TRACKING LOST", (75,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),2)
-            cv2.putText(frame_left, "TRACKING LOST", (75,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),2)
+        # If cannot catch any frame, break
+        if not succes_right or not succes_left:                    
+            break
 
         else:
-            # Function to calculate depth of object. Outputs vector of all depths in case of several balls.
-            # All formulas used to find depth is in video presentaion
-            depth = tri.find_depth(center_point_right, center_point_left, frame_right, frame_left, B, f, alpha)
 
-            cv2.putText(frame_right, "Distance: " + str(round(depth,1)), (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0,255,0),3)
-            cv2.putText(frame_left, "Distance: " + str(round(depth,1)), (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0,255,0),3)
-            # Multiply computer value with 205.8 to get real-life depth in [cm]. The factor was found manually.
-            print("Depth: ", str(round(depth,1)))
+            start = time.time()
+            
+            # Convert the BGR image to RGB
+            frame_right = cv2.cvtColor(frame_right, cv2.COLOR_BGR2RGB)
+            frame_left = cv2.cvtColor(frame_left, cv2.COLOR_BGR2RGB)
+            
+            # Process the image and find faces
+            results_right = face_detection.process(frame_right)
+            results_left = face_detection.process(frame_left)
 
-
-
-        end = time.time()
-        totalTime = end - start
-
-        fps = 1 / totalTime
-        #print("FPS: ", fps)
-
-        cv2.putText(frame_right, f'FPS: {int(fps)}', (20,450), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0,255,0), 2)
-        cv2.putText(frame_left, f'FPS: {int(fps)}', (20,450), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0,255,0), 2)                                   
+            # Convert the RGB image to BGR
+            frame_right = cv2.cvtColor(frame_right, cv2.COLOR_RGB2BGR)
+            frame_left = cv2.cvtColor(frame_left, cv2.COLOR_RGB2BGR)
 
 
-        # Show the frames
-        cv2.imshow("frame right", frame_right) 
-        cv2.imshow("frame left", frame_left)
+            ################## CALCULATING DEPTH #########################################################
+
+            center_right = 0
+            center_left = 0
+
+            if results_right.detections:
+                for id, detection in enumerate(results_right.detections):
+                    mp_draw.draw_detection(frame_right, detection)
+
+                    bBox = detection.location_data.relative_bounding_box
+
+                    h, w, c = frame_right.shape
+
+                    boundBox = int(bBox.xmin * w), int(bBox.ymin * h), int(bBox.width * w), int(bBox.height * h)
+
+                    center_point_right = (boundBox[0] + boundBox[2] / 2, boundBox[1] + boundBox[3] / 2)
+
+                    cv2.putText(frame_right, f'{int(detection.score[0]*100)}%', (boundBox[0], boundBox[1] - 20), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,255,0), 2)
 
 
-        # Hit "q" to close the window
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+            if results_left.detections:
+                for id, detection in enumerate(results_left.detections):
+                    mp_draw.draw_detection(frame_left, detection)
+
+                    bBox = detection.location_data.relative_bounding_box
+
+                    h, w, c = frame_left.shape
+
+                    boundBox = int(bBox.xmin * w), int(bBox.ymin * h), int(bBox.width * w), int(bBox.height * h)
+
+                    center_point_left = (boundBox[0] + boundBox[2] / 2, boundBox[1] + boundBox[3] / 2)
+
+                    cv2.putText(frame_left, f'{int(detection.score[0]*100)}%', (boundBox[0], boundBox[1] - 20), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,255,0), 2)
+
+
+
+
+            # If no ball can be caught in one camera show text "TRACKING LOST"
+            if not results_right.detections or not results_left.detections:
+                cv2.putText(window, "TRACKING LOST", (800,280), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),2)
+                cv2.putText(window, "TRACKING LOST", (80,280), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),2)
+
+            else:
+                
+                # Function to calculate depth of object. Outputs vector of all depths in case of several balls.
+                # All formulas used to find depth is in video presentaion
+                depth = tri.find_depth(center_point_right, center_point_left, frame_right, frame_left, B, f, alpha)
+
+                cv2.putText(window, "Distance: " + str(round(depth,1))+" cm", (800,280), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0),3)
+                cv2.putText(window, "Distance: " + str(round(depth,1))+" cm", (80,280), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0),3)
+                # Multiply computer value with 205.8 to get real-life depth in [cm]. The factor was found manually.
+                # print("Depth: ", str(round(depth,1)))
+
+
+
+            end = time.time()
+            totalTime = end - start
+
+            fps = 1 / totalTime
+            #print("FPS: ", fps)
+
+            cv2.putText(window, f'FPS: {int(fps)}', (800,240), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0,255,0), 2)
+            cv2.putText(window, f'FPS: {int(fps)}', (80,240), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0,255,0), 2)                                   
+            cv2.putText(window, 'Right Frame', (800,200), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0,255,0), 2)
+            cv2.putText(window, 'Left Frame', (80,200), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0,255,0), 2)
+            # Show the frames
+            window[300:300+480,80:80+640]=frame_left
+            window[300:300+480,800:800+640]=frame_right
+            Hori = np.concatenate((frame_right, frame_left), axis=1)
+            cv2.imshow("Images", window) 
+           
+
+
+            # Hit "q" to close the window
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
 
 # Release and destroy all windows before termination
